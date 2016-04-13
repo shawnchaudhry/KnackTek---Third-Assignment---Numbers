728ddf0cee2b4c1a82a4e4c6ba8220a06b5234b4
Shawn Chaudhry <shawnchaudhry@hotmail.com>
2016-04-12 23:27:28 -0400

    Prints 1 through 100. Someone earlier asked me for the code in it's entirety, so here it is!

---
+++ KnackTek - Third Assignment - Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnackTek___Third_Assignment___Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while(i < 101)
+            {
+                Console.WriteLine("Number " + i + ": " + i);
+                i++;
+            }
+        }
+    }
+}
